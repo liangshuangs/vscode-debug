f04481b4f8d0af96341cf42e93f8cb5c6528c429
liangshuang15 <liangshuang15@macdeMacBook-Pro.local>
2022-09-06 14:53:18 +0800

    first commit3

---
+++ demo1/app.js
@@ -0,0 +1,9 @@
+/*
+ * @Anthor: liangshuang15
+ * @Description:
+ * @Date: 2022-09-06 12:05:41
+ * @LastEditTime: 2022-09-06 14:20:49
+ * @FilePath: /vscode-debug/demo1/app.js
+ */
+const meg = 'Hello World';
+console.log(meg)
